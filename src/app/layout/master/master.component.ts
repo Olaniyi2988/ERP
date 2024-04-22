0920b68e5406c3dd67d60b5a1245c9899de3029c
Olaniyi2988 <psimeon360@gmail.com>
2024-04-22 15:22:15 +0100

    navbar update

--- src/app/layout/master/master.component.ts
+++ src/app/layout/master/master.component.ts
@@ -3,11 +3,12 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { SearchbarComponent } from '../../components/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-master',
   standalone: true,
-  imports: [CommonModule, RouterModule, SidebarComponent, NavbarComponent],
+  imports: [CommonModule, RouterModule, SidebarComponent, NavbarComponent, SearchbarComponent],
   templateUrl: './master.component.html',
   styleUrl: './master.component.scss'
 })
