9e6f939af49c0eaceea948975ec731fcc812632d
Olaniyi2988 <psimeon360@gmail.com>
2024-04-24 13:55:33 +0100

    sidebar update

---
+++ src/app/components/table-one/table-one.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-one',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-one.component.html',
+  styleUrl: './table-one.component.scss'
+})
+export class TableOneComponent {
+
+}
